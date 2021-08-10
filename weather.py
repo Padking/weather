d16ec9f79d1d4a5dd5eda4ec5341085f2884cffa
Pavel Dubov <PADking888@yandex.ru>
2021-08-10 20:53:56 +0300

    feat: step 4 and 5 get weather in three cities

--- weather.py
+++ weather.py
@@ -1,10 +1,34 @@
+from typing import Dict
+
 import requests
 
 
-url = "https://wttr.in/san%20francisco"
-payload = {"nTqu": "", "lang": "en"}
+URLOptions = Dict[str, str]
 
-response = requests.get(url, params=payload)
-response.raise_for_status()
+cities = ["Лондон", "Шереметьево", "Череповец"]
+payload = {"mnTqu": "", "lang": "ru"}
 
-print(response.text)
+
+def get_weather_forecast_info(city: str, payload: URLOptions):
+    """
+    Выполняет http-з для получения информации
+    о погоде в конкретном городе с учётом опций (согласно http://wttr.in)
+
+    :param location: местоположение
+    :param payload: опции
+
+    """
+
+    url_template = "http://wttr.in/{}?{}"
+    url = url_template.format(city, payload)
+
+    response = requests.get(url, params=payload)
+    response.raise_for_status()
+
+    info = response.text
+    return info
+
+
+for city in cities:
+    info = get_weather_forecast_info(city, payload)
+    print(info)
